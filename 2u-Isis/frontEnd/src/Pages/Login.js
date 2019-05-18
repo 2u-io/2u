e60131cb1cde3242f7f843678445162fa1c99195
i21ng <garcia.isisnayely@gmail.com>
2019-05-18 11:59:47 -0500

    login

---
+++ 2u-Isis/frontEnd/src/Pages/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import signUp from '../Components/SignUp';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <signUp/>
+      </React.Fragment>
+    )
+  }
+}
