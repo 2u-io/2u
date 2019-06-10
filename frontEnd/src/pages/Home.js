873817a781c69efa791a0057465787145b7b7533
francisco gutierrez <fockus.gutierrez@gmail.com>
2019-06-09 22:45:54 -0500

    imagenes
    coleccion de imagenes para trabajar con ellas en photoshop y anexarlas a la pagina despues

--- frontEnd/src/pages/Home.js
+++ frontEnd/src/pages/Home.js
@@ -7,6 +7,7 @@ import regalo from '../Components/image/new-product.png';
 import tienda from '../Components/image/retail.png';
 import mapa from '../Components/image/map-location.png';
 import present from '../Components/image/present.png';
+import nosotros from '../Components/image/nosotros.jpg'
 
 
 export default class Home extends Component {
@@ -39,25 +40,30 @@ export default class Home extends Component {
         </div>
         <div className="iconos">
           <div className="regalo">
-          <img src={regalo} alt="" className="iconosSection"/>
-          <p className="subtitle">Choose your gifts</p>
+            <img src={regalo} alt="" className="iconosSection"/>
+            <p className="subtitle">Choose your gifts</p>
           </div>
           <div className="regalo">
-          <img src={tienda} alt="" className="iconosSection"/>
-          <p className="subtitle">Choose your gifts</p>
+            <img src={tienda} alt="" className="iconosSection"/>
+            <p className="subtitle">Choose your gifts</p>
           </div>
           <div className="regalo">
-          <img src={mapa} alt="" className="iconosSection"/>
-          <p className="subtitle">Choose your gifts</p>
+            <img src={mapa} alt="" className="iconosSection"/>
+            <p className="subtitle">Choose your gifts</p>
           </div>
           <div className="regalo">
-          <img src={present} alt="" className="iconosSection"/>
-          <p className="subtitle">Choose your gifts</p>
-          </div>
-            
+            <img src={present} alt="" className="iconosSection"/>
+            <p className="subtitle">Choose your gifts</p>
+          </div>            
         </div>
+        <section>
+          <div className="nosotros">
+            <img src={nosotros} alt="" className="imgNosotros"/>
+            <p className="texNosotros"><span>“</span>Somos un grupo de personas con pasion por el desarrollo web y nuevas tecnologias para el desarrollo... Nuestro proyecto fue inspirado en las necesidades que surgen al momento de querer hacer algun obsequio para algna persona querida o especial, y no tienes el tiempo de hacer una compra de ese regalo o querer dar esa sorpresa a la persona que tanto quieres. <span>”</span></p>
+          </div>
+        </section>
 
-        <Link to='/catalogo' className='btn'>Empezar</Link>
+        {/* <Link to='/catalogo' className='btn'>Empezar</Link> */}
       </React.Fragment>
     )
   }
