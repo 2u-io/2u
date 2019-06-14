f5d196e2032ab281929421862454c3ab6f60477e
Cristian Eunice Salazar Cortés <salazar.cortes.cristian@outlook.com>
2019-06-14 16:37:12 -0500

    Fixed path for React build folder

--- backEnd/index.js
+++ backEnd/index.js
@@ -26,12 +26,12 @@ require('./routes/tiesRoutes')(app);
 require('./routes/wrappingsRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('frontEnd/build'));
 
     const path = require('path');
 
     app.get('*', (request, response) => {
-        response.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+        response.sendFile(path.resolve(__dirname, '../frontEnd', 'build', 'index.html'));
     });
 }
 
